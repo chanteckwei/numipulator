248164548b2775375471d921c27cafab80fa758d
Chan Teck Wei <chan.tw97@gmail.com>
2019-07-11 16:18:09 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='numipulator',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/chantw/numipulator',
+    license='Apache License 2.0',
+    author='Chan Teck Wei',
+    author_email='chan.tw97@gmail.com',
+    description='Numpy-based image manipulation library.', install_requires=['numpy', 'scipy']
+)
