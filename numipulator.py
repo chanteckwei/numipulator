21090f515439b3267ff393898d0bde9d237feb5d
Chan Teck Wei <chan.tw97@gmail.com>
2019-07-18 09:51:42 +0800

    Update version to 0.2.1

--- numipulator.py
+++ numipulator.py
@@ -1,7 +1,7 @@
 from typing import Tuple
 
 __all__ = ['NImage']
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 import numpy as np
 from PIL import Image
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='numipulator',
-    version='0.2.0',
+    version='0.2.1',
     packages=[''],
     url='https://github.com/chantw/numipulator',
     license='Apache License 2.0',
